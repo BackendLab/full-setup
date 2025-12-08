6b0bce0914a79f1379267715c2836b09d217dd64
Akhil-Nagpal <akhilnagpal07@gmail.com>
2025-12-08 22:35:36 +0530

    added custom asynchandler

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,13 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const asyncHandler =
+  (
+    requestHandler: (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => Promise<any>
+  ) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
