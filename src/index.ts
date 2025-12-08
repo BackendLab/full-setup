8dd2b4c3131d93f7db2ae3400b3174beb2724b09
Akhil-Nagpal <akhilnagpal07@gmail.com>
2025-12-08 17:36:22 +0530

    established servcer connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import { connectDB } from "./db/connect";
+import { app } from "./app";
 
-connectDB();
+const Port = Bun.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen((Port) => {
+      console.log("Server is running on Port:", Port);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed!", error);
+  });
