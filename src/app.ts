a83bc746bf0bd54d19b725d6cc1e6f8ab52cb0d0
Akhil-Nagpal <akhilnagpal07@gmail.com>
2025-12-08 16:24:48 +0530

    server connection established

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+export const app = express();
+
+// middleware for express json limit
+app.use(express.json({ limit: "16kb" }));
+
+// middleware for cors origin
+app.use(
+  cors({
+    origin: Bun.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// middleware for Url Encoding - example - this change the link in the browser like akhil+nagpal or akhil%20nagpal
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// middleware for cookie-parser
+app.use(cookieParser());
