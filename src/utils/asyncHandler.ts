f3581b59e2f3f6c7bfdbee8f841068b8ed2f5d29
Akhil-Nagpal <akhilnagpal07@gmail.com>
2025-12-09 17:12:11 +0530

    added async await asyncHandler method

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 
+// Promise based higher order arrow function - production grade way
 export const asyncHandler =
   (
     requestHandler: (
@@ -11,3 +12,17 @@ export const asyncHandler =
   (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
+
+// Async Await based higher order arrow function - not very used in production grade but its a good way
+// export const asyncHandler =
+//   (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       await fn(req, res, next);
+//     } catch (error: any) {
+//       res.status(error.statusCode || 500).json({
+//         success: false,
+//         message: error.message,
+//       });
+//     }
+//   };
