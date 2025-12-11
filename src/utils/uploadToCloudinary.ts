bd3250f8d2c8ca0d4e966d5d9cb6fd073285cd77
Akhil-Nagpal <akhilnagpal07@gmail.com>
2025-12-11 19:15:19 +0530

    added upload method

---
+++ src/utils/uploadToCloudinary.ts
@@ -0,0 +1,28 @@
+// What I need to do -
+// write async function to add all the images and files to cloudinary
+// add file system to handle the upload and removing the unnecessary file
+
+import cloudinary from "../config/cloudinary";
+import fs from "fs";
+
+const uploadToCloudinary = async (localFilePath: string) => {
+  try {
+    // checking if the local file path exists or not
+    if (!localFilePath) return null;
+    // adding file to cloudinary
+    const result = cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // removing file from the local storage
+    fs.unlinkSync(localFilePath);
+    // returning the result
+    return result;
+  } catch (error) {
+    // if the uploading fails then also remove the file from storage
+    fs.unlinkSync(localFilePath);
+    console.error(error);
+    return null;
+  }
+};
+
+export { uploadToCloudinary };
