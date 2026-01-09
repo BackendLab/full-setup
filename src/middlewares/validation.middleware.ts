11fbbe458ef4f33bc731ad9f6fc263d8141ca8d6
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-01-10 00:30:50 +0530

    refactor: update validation middleware to use ZodType for schema validation

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,12 +1,11 @@
-import type { NextFunction, Request } from "express";
-import type { AnyZodObject } from "zod/v3";
+import type { NextFunction, Request, Response } from "express";
+import type { ZodType } from "zod";
 
 // What this middleware do is validate the zod schema in the route before sending request to controller
 // This function simply parse the upcoming request and check whether the data is correct and expected or not
 
 export const validate =
-  (schema: AnyZodObject) =>
-  (req: Request, _res: Response, next: NextFunction) => {
+  (schema: ZodType) => (req: Request, _res: Response, next: NextFunction) => {
     schema.parse({
       body: req.body,
       params: req.params,
