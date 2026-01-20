9136dd8b3294e24d3435cc4ff27340ae6684d0ac
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-01-20 18:38:08 +0530

    feat: enhance getChannelProfile functionality with channel state validation and subscription check

--- src/routes/channel.routes.ts
+++ src/routes/channel.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware";
 import { getChannelProfile } from "../controllers/channel.controller";
+import { validate } from "../middlewares/validation.middleware";
+import { channelParamSchema } from "../validations/channel.validation";
+import { checkChannelState } from "../middlewares/channelState.middleware";
 
 const router = Router();
 
 // Channel Routes
-router.get("/channel/:channelId", verifyJwt, getChannelProfile);
+router.get(
+  "/channel/:channelId",
+  verifyJwt,
+  checkChannelState,
+  validate(channelParamSchema),
+  getChannelProfile
+);
 
 export default router;
--- src/services/channel.service.ts
+++ src/services/channel.service.ts
@@ -1,6 +1,38 @@
 import type { Request, Response } from "express";
+import { Types } from "mongoose";
+import { Channel } from "../models/channel.model";
+import { ApiError } from "../utils/apiError";
+import { Subscription } from "../models/subscription.model";
 
 export const getChannelProfileService = async (
   viewerId: string,
   channelId: string
-) => {};
+) => {
+  // get the channel and viewer Id and cover it to ObjectId
+  const channelObjectId = new Types.ObjectId(channelId);
+
+  const viewerObjectId = new Types.ObjectId(viewerId);
+  // fetch the channel
+  const channel = await Channel.findOne({
+    _id: channelObjectId,
+    status: "ACTIVE",
+  });
+  // check if the channel exist or not
+  if (!channel) {
+    throw new ApiError(404, "Channle not found");
+  }
+  // check if the viewer subscribed to this channel or not
+  let isSubscribed = false;
+
+  if (viewerObjectId) {
+    // NOTE: This !! is
+    const subscriptionExists = !!(await Subscription.exists({
+      channel: channelObjectId,
+      subscriber: viewerObjectId,
+    }));
+
+    isSubscribed = subscriptionExists;
+  }
+  // add playlist with videos - Aggregation Pipeline
+  // return the result
+};
