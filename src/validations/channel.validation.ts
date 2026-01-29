9299983b914dacf308cc244a27cb90768d938728
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-01-30 01:12:14 +0530

    feat: add update channel info schema with validation for channelId

--- src/validations/channel.validation.ts
+++ src/validations/channel.validation.ts
@@ -13,6 +13,16 @@ export const channelStateSchema = z.object({
   state: z.enum(ChannelState),
 });
 
+// Update Channel Info Schema
+export const updateChannelInfoSchema = z.object({
+  params: z.object({
+    channelId: z.string().regex(/^[0-9a-fA-F]{24}$/, "Inavlid ChannelId"),
+  }),
+  file: z.object({
+    path: z.string(),
+  }),
+});
+
 // Channel Avatar Schema
 export const channelAvatarSchema = z.object({
   params: z.object({
