c5f060431bfe5dff518ba48a7e5c52cd43be8f76
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-03-09 21:10:42 +0530

    added zod validation for uploading video

--- src/validations/video.validation.ts
+++ src/validations/video.validation.ts
@@ -5,3 +5,11 @@ export const videoParamSchema = z.object({
     videoId: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid Video ID"),
   }),
 });
+
+export const UploadVideoSchema = z.object({
+  videoFile: z.object({
+    url: z.url(),
+    publicId: z.string(),
+  }),
+  duration: z.number(),
+});
